e5523fb8dce19c628e070f46a0263b5cd1a47daf
Atholliez <32449515+Atholliez@users.noreply.github.com>
2017-11-30 14:54:17 +0100

    tache 2 OK

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,17 +1,24 @@
 package org.ulco;
 
+import java.lang.reflect.Constructor;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
 
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        type = type.substring(0, 1).toUpperCase() + type.substring(1);
+
+        Class c;
+        try {
+            c = Class.forName("org.ulco."+ type);
+            Class[] parametres = new Class[]{String.class};
+            Constructor constru = c.getConstructor(parametres);
+            o = (GraphicsObject)constru.newInstance(new String[]{str});
+        }
+        catch (Exception except) {
+            except.printStackTrace();
         }
         return o;
     }
@@ -19,11 +26,9 @@ public class JSON {
     static public Group parseGroup(String json) {
         return new Group(json);
     }
-
     static public Layer parseLayer(String json) {
         return new Layer(json);
     }
-
     static public Document parseDocument(String json) {
         return new Document(json);
     }
